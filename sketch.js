78182dacaa3b00260c53a5b305d3b4ef8e040a83
Dyl-i-media <dylan2.chappell@live.uwe.ac.uk>
2025-12-19 15:52:52 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,3 @@
-// Contrast: Radial Particle System
-// Inspired by algorithmic generative art
-// Theme: Contrast using conditionals and mouse interaction
 
 let points = [];
 let numPoints = 600;
@@ -11,7 +8,7 @@ function setup() {
   angleMode(DEGREES);
   background(0);
 
-  // Create points around a circle
+ 
   for (let i = 0; i < numPoints; i++) {
     let angle = random(360);
     let r = random(radius * 0.3, radius);
@@ -20,7 +17,7 @@ function setup() {
 }
 
 function draw() {
-  background(0, 30); // slight trail effect
+  background(0, 30); // trail effect
 
   translate(width / 2, height / 2);
 
@@ -32,25 +29,18 @@ function draw() {
     let x = cos(p.angle) * p.r;
     let y = sin(p.angle) * p.r;
 
-    // CONTRAST using conditionals
+   
     if (mouseDist < radius) {
-      stroke(255, 220, 0); // bright yellow
+      stroke(255, 220, 0); 
       strokeWeight(2);
     } else {
-      stroke(80); // dark grey (low contrast)
+      stroke(80); 
       strokeWeight(1);
     }
 
     point(x, y);
 
-    // Subtle rotation
     p.angle += 0.2;
   }
 }
 
-// Optional save
-function keyPressed() {
-  if (key === 's' || key === 'S') {
-    saveCanvas('contrast_radial', 'png');
-  }
-}
